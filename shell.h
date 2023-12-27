c64c85354fed24be0e54e6db23bd647b2552a195
ziyadekanssi <ziyade577@gmail.com>
2023-12-27 06:21:53 -0800

    alx

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef shell_h
+#define shell_h
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#define CUSTOM_DELIM " \t\n"
+ 
+extern char **custom_environ;
+char *custom_read_line(void);
+char **my_tokenizer(char *line);
+int execute_alternative(char **alternative_command, char **argv);
+#endif
